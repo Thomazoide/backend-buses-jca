773d8f5ba8bfb815abde3d767896abc63792e8e9
Thomazoide ubuntu <ttellerias01@outlook.com>
2024-09-25 15:33:49 -0300

    bug fix on bus.service

--- src/services/bus.service.ts
+++ src/services/bus.service.ts
@@ -64,7 +64,7 @@ export class BusService{
     }
 
     async GetUnassignedBuses(): Promise<Bus[]>{
-        return await this.repositorio.findBy({chofer: IsNull()})
+        return await this.repositorio.findBy({chofer: null})
     }
 
     async DeleteBus(bus: Partial<Bus>): Promise<ResponsePayload>{
